a2324c3f5c6deb834472bdac645df70da6509fb5
Yi Li <yil8@uci.edu>
2016-04-26 12:16:34 -0700

    tomtom.sh: RNA motif db added

--- consdeep/tomtom.sh
+++ consdeep/tomtom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-~/Programs/MEME/meme_4.11.1/bin/tomtom -no-ssc -oc ./$2 -verbosity 2 -min-overlap 5 -dist pearson -evalue -thresh 0.01 $1 ~/Programs/MEME/meme_4.11.1/db/jolma2013.meme ~/Programs/MEME/meme_4.11.1/db/JASPAR_CORE_2016_vertebrates.meme ~/Programs/MEME/meme_4.11.1/db/uniprobe_mouse.meme
+~/Programs/MEME/meme_4.11.1/bin/tomtom -no-ssc -oc ./$2 -verbosity 2 -min-overlap 5 -dist pearson -evalue -thresh 0.01 $1 ~/Programs/MEME/meme_4.11.1/db/jolma2013.meme ~/Programs/MEME/meme_4.11.1/db/JASPAR_CORE_2016_vertebrates.meme ~/Programs/MEME/meme_4.11.1/db/uniprobe_mouse.meme ~/Programs/MEME/meme_4.11.1/db/Ray2013_rbp_Homo_sapiens.dna_encoded.meme ~/Programs/MEME/meme_4.11.1/db/Homo_sapiens.dna_encoded.meme
 
